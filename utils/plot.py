a213d43c21f344811575b0f0644aded032cd0ba6
Kunwoo Na <jamongna@gmail.com>
2022-06-28 16:00:01 +0900

    Implemented display functionality.

---
+++ utils/plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+
+
+def display_progress(real_image, fake_image, path, \
+    figsize = (12, 6)) : 
+    real_image = real_image.detach().cpu()
+    fake_image = fake_image.detach().cpu()
+    fig, ax = plt.subplot(1, 2, figsize = figsize)
+    ax[0].imshow(real_image)
+    ax[1].imshow(fake_image)
+    ax[0].title.set_text('Input image')
+    ax[1].title.set_text('Result image')
+    plt.axis('off')
+    plt.show()
+    plt.savefig(path)
